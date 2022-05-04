5db0acdd3dece2918b03b535ee86399c5814c108
EC2 Default User <ec2-user@ip-172-31-47-93.ap-northeast-1.compute.internal>
2022-05-04 16:05:46 +0000

    残業beforeOk

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,10 +1,13 @@
 class AttendancesController < ApplicationController
   include AttendancesHelper
   
-  before_action :set_user, only:[:edit_one_month_request,:update_one_month_request ,:edit_one_month_notice, :update_one_month_notice]
-  before_action :logged_in_user, only:[:update,:edit_one_month_request,:update_one_month_request, :edit_one_month_notice, :update_one_month_notice,:working]
+  before_action :set_user, only:[:edit_one_month_request,:update_one_month_request ,:edit_one_month_notice, :update_one_month_notice,
+                                 :edit_overtime_notice, :update_overtime_notice]
+  before_action :logged_in_user, only:[:update,:edit_one_month_request,:update_one_month_request, :edit_one_month_notice, :update_one_month_notice,
+                                       :edit_overtime_request, :update_overtime_request, :edit_overtime_notice, :update_overtime_notice, :working]
   before_action :general_user, only:[:update,:edit_one_month_request,:update_one_month_request ]
-  before_action :correct_user, only:[:update,:edit_one_month_request,:update_one_month_request, :edit_one_month_notice, :update_one_month_notice]
+  before_action :correct_user, only:[:update,:edit_one_month_request,:update_one_month_request, :edit_one_month_notice, :update_one_month_notice,
+                                     :edit_overtime_request, :update_overtime_request, :edit_overtime_notice, :update_overtime_notice ]
   before_action :admin_user, only:[:working]
   before_action :set_one_month, only:[:edit_one_month_request]
   
